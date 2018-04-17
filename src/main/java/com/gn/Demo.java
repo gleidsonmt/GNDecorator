b2670477dd87a33b03afcc67f5554f1bac3c9443
Gleidson <gleidisonmt@gmail.com>
2018-04-17 18:12:21 -0300

    solving conflict

---
+++ src/main/java/com/gn/Demo.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.gn;
+
+import com.gn.options.ButtonType;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ */
+public class Demo extends Application {
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXML.fxml"));
+
+        Region region = new Region();
+        
+        GNDecorator window = new GNDecorator();
+        window.setTitle("GNDecoration");
+        window.setContent(root);
+        window.addButton(ButtonType.FULL_EFFECT);
+        window.initTheme(GNDecorator.Theme.DARKULA);
+        window.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private Object ButtonType(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ src/main/java/com/gn/NewFXMain.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2018 Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.gn;
+
+import com.gn.buttons.Close;
+import com.gn.buttons.Watermark;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ */
+public class NewFXMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        
+        Close control = new Close();
+        
+        
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        Watermark root = new Watermark();
+        root.getStyleClass().add("tedo");
+//        root.setAlignment(Pos.BASELINE_LEFT);
+        root.getChildren().addAll(control, btn);
+
+        Scene scene = new Scene(root, 300, 250);
+        
+        scene.getStylesheets().add(getClass().getResource("/css/controls/teste.css").toExternalForm());
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
