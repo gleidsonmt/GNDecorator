5e46e83fcc14475aea793fc31b8eeaedebcbc767
Gleidson <gleidisonmt@gmail.com>
2020-06-24 14:35:21 -0300

    add codes

---
+++ src/main/java/io/github/Gleidson28/Test.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.Gleidson28;
+
+import io.github.Gleidson28.decorator.GNDecorator;
+import io.github.Gleidson28.decorator.buttons.ButtonTest;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
+import javafx.stage.Stage;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  23/09/2018
+ * Version 1.0
+ */
+public class Test extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+//        Parent root = FXMLLoader.load(getClass().getResource("/com/gn/resources/sample.fxml"));
+
+        VBox content = new VBox();
+        content.setAlignment(Pos.CENTER);
+        content.setPrefSize(400,400);
+        GNDecorator window = new GNDecorator();
+
+//        window.hideCustoms();
+        Button btn = new Button("Action");
+        btn.setOnMouseClicked(event ->{
+//            window.removeControls();
+            window.floatActions();
+//            window.hideControls();
+
+//            System.out.println("controls " + window.getControls());
+//            window.showCustoms();
+//            window.block();
+//            window.setResizable(true);
+        });
+
+//        window.addButton(ButtonType.FULL_EFFECT);
+        content.getChildren().add(btn);
+        window.setContent(content);
+
+
+        window.setTitle("Application");
+//        window.addButton(ButtonType.FULL_EFFECT);
+        window.setTitle(null);
+
+        ButtonTest a1 = new ButtonTest("Button 1");
+        ButtonTest a2 = new ButtonTest("Button 2");
+        ButtonTest a3 = new ButtonTest("Button 3");
+
+
+        window.addControl(2, a1);
+        window.addControl(4,a2);
+        window.addControl(a3);
+
+
+        Menu menu = new Menu("File");
+        menu.getItems().add(new MenuItem("Open"));
+        menu.getItems().add(new MenuItem("Close"));
+
+        Menu menu1 = new Menu("Save");
+        menu1.getItems().add(new MenuItem("Save"));
+        menu1.getItems().add(new MenuItem("Save as"));
+
+        Menu menu2 = new Menu("About");
+        menu2.getItems().add(new MenuItem("About us"));
+        menu2.getItems().add(new MenuItem("Exit"));
+
+        Menu btn_ico = new Menu();
+        btn_ico.setStyle("-fx-background-color : transparent;");
+        btn_ico.show();
+        btn_ico.getItems().clear();
+
+//        System.out.println(btn_ico.fire());
+
+        SVGPath icon = new SVGPath();
+        icon.setId("icon");
+        icon.setContent("M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z");
+        btn_ico.setGraphic(icon);
+        icon.setFill(Color.web("#999"));
+        icon.setOnMouseClicked(event -> System.out.println("teste"));
+
+        window.addMenu(btn_ico);
+        window.addMenu(menu);
+        window.addMenu(2,menu1);
+        window.addMenu(menu2);
+
+
+//        window.setMaximized(true);
+        window.fullBody();
+//        window.addCustom(new FullScreen());
+//        window.centralizeTitle();
+        window.show();
+
+//        window.getScene().getStylesheets().addAll(getClass().getResource("/com/gn/resources/css/custom.css").toExternalForm());
+
+//        ScenicView.show(window.getScene());
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/io/github/Gleidson28/decorator/buttons/Close.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2018 Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.Gleidson28.decorator.buttons;
+
+import io.github.Gleidson28.decorator.background.GNBackground;
+import com.sun.javafx.css.converters.PaintConverter;
+import com.sun.javafx.scene.control.skin.ButtonSkin;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javafx.css.CssMetaData;
+import javafx.css.SimpleStyleableObjectProperty;
+import javafx.css.Styleable;
+import javafx.css.StyleableObjectProperty;
+import javafx.css.StyleableProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Skin;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+/**
+ * @author   Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Creation  15/04/2018
+ */
+public class Close extends Button {
+    
+    private static final String USER_AGENT_STYLESHEET = GNBackground.class.getResource("/css/controls/buttons.css").toExternalForm();
+    private final ImageView viewClose = new ImageView(new Image("/img/close.png"));
+    
+    public Close(){
+        getStyleClass().add("gn-close");
+        super.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+        super.setGraphic(viewClose);
+    }
+    
+    @Override
+    protected Skin<?> createDefaultSkin() {
+        return new ButtonSkin(Close.this);
+    }
+
+    @Override
+    public String getUserAgentStylesheet() {
+        return USER_AGENT_STYLESHEET;
+    }
+
+    private final StyleableObjectProperty<Paint> defaultFill = new SimpleStyleableObjectProperty<>(StyleableProperties.DEFAULT_FILL,
+            Close.this,
+            "defaultFill",
+            Color.WHITE);
+
+    public Paint getDefaultFill() {
+        return defaultFill.get();
+    }
+
+    public StyleableObjectProperty<Paint> defaultFillProperty() {
+        return this.defaultFill;
+    }
+
+    public void setDefaultFill(Paint color) {
+        this.defaultFill.set(color);
+        this.setStyle("-gn-fill : white");
+    }
+
+    private static class StyleableProperties {
+
+        private static final CssMetaData<Close, Paint> DEFAULT_FILL
+                = new CssMetaData<Close, Paint>("-gn-fill",
+                        PaintConverter.getInstance(), Color.RED) {
+            @Override
+            public boolean isSettable(Close control) {
+                return control.defaultFill == null || !control.defaultFill.isBound();
+            }
+
+            @Override
+            public StyleableProperty<Paint> getStyleableProperty(Close control) {
+                return control.defaultFillProperty();
+            }
+        };
+
+        private static final List<CssMetaData<? extends Styleable, ?>> CHILD_STYLEABLES;
+
+        static {
+            final List<CssMetaData<? extends Styleable, ?>> styleables
+                    = new ArrayList<>(Button.getClassCssMetaData());
+            Collections.addAll(styleables,
+                    DEFAULT_FILL);
+            CHILD_STYLEABLES = Collections.unmodifiableList(styleables);
+        }
+    }
+
+    // inherit the styleable properties from parent
+    private List<CssMetaData<? extends Styleable, ?>> STYLEABLES;
+
+    @Override
+    public List<CssMetaData<? extends Styleable, ?>> getControlCssMetaData() {
+        if (STYLEABLES == null) {
+            final List<CssMetaData<? extends Styleable, ?>> styleables
+                    = new ArrayList<>(Button.getClassCssMetaData());
+            styleables.addAll(getClassCssMetaData());
+            styleables.addAll(Close.getClassCssMetaData());
+            STYLEABLES = Collections.unmodifiableList(styleables);
+        }
+        return STYLEABLES;
+    }
+
+    public static List<CssMetaData<? extends Styleable, ?>> getClassCssMetaData() {
+        return StyleableProperties.CHILD_STYLEABLES;
+    }
+
+}
